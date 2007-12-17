0568bc3ef1619c9b72a352e5b9b07d51d0575fb0
s.stpettersen <s.stpettersen@fc3e1cb1-3f40-0410-be83-bb94d4db2189>
2007-12-17 17:20:13 +0000

    Changed directory structure for modules.

---
+++ chrome/platform/modules/core/msh/base.lua
@@ -0,0 +1,37 @@
+-------------------------------------------
+-- Moonshine environment for Lua-based
+-- services in Mozilla Firefox web browser
+-- Copyright (c) 2007 Sam Saint-Pettersen.
+
+-- This program is released "as is" under
+-- the GNU General Public License V3.
+-------------------------------------------
+-- specify as module
+module(..., package.seeall)
+
+-- return version information
+function version()
+    return "Base module for Moonshine 0.1\nCopyright (c) 2007 Sam Saint-Pettersen"
+end
+
+-- load an msh application;
+-- invoked from msh core interpreter
+function loadMSH(file)
+	local mshapp = xml.load(file)
+	--if file == extension of msh
+		-- begin parsing
+		parseMetadata(mshapp)
+		parseLuacode(mshapp)
+	--else 
+		-- return an error message
+		return "Error. This is not an MSH application!"
+end
+
+-- parse msh application metadata
+function parseMetadata(mshapp)
+
+end
+-- parse application code
+function parseLuacode(mshapp)
+
+end
---
+++ chrome/platform/modules/core/xml/parser.lua
@@ -0,0 +1,59 @@
+----------------------------------------------------
+-- XML parser for use with XML in Lua programs
+-- Version 0.1
+-- Copyright (c) 2007 Sam Saint-Pettersen
+-- Released under the GNU General Public License V3
+----------------------------------------------------
+
+-- specify as module
+module(..., package.seeall)
+
+-- allowed characters as tag value
+tagval = "[%.%,%;%:%-%&%%\\%\/%#%~%\"%@%\'%w%x%s]+"
+
+-- allowed characters as attribute value
+attval = "[%.%,%;%:%-%&%@%w%x%s]+"
+
+-- return version information
+function version()
+	return "XML Parser 0.1"
+end
+-- load file
+function load(file)
+	if file ~= nil then
+		local xml = io.open(file, "r")
+		return (xml:read("*all"))
+	else
+		return "Usage: xml.load(file as str)"
+	end
+end
+-- parse tag
+function tag(xml, tag, occur)
+         if xml or tag ~= nil then
+                local tagp = tagval .. "</" .. tag .. ">"
+                local tagm = string.match(xml, tagp)
+                local start = string.len(tag) - 1000
+                if tagm ~= nil then
+                        return string.match(tagm, tagval, start)
+                else
+                        return "Tag: " .. tag .. " does not exist."
+                end
+         else
+                return "Usage: xml.stag(xml as str, tag as str [, occur as int])"
+         end
+end
+-- parse attribute
+function attrib(xml, tag, attr, occur)
+	if xml or tag or attr ~= nil then
+		local attp = "<" .. tag .. "%s" .. attr .. "%=%\"?%\'?" .. attval .. "%\'?%\"?%\/?>"
+		local attm = string.match(xml, attp)
+		local start = string.len(tag) + string.len(attr) + 3
+		if attm ~= nil then
+			return string.match(attm, attval, start)
+		else
+			return "Attribute: " .. attr .. " does not exist for " .. tag .. "."
+		end
+	else
+		return "Usage: xml.attrib(xml as str, tag as str, attr as str [, occur as int])"
+	end
+end
