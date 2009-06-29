98db6af18a287bb86c47de9c6247a49e51cd27c0
s.stpettersen <s.stpettersen@fc3e1cb1-3f40-0410-be83-bb94d4db2189>
2009-06-29 14:06:03 +0000

    Fixed, so can be built with Gecko SDK 1.9. :D Thanks to @paul_rouget.

--- trunk/xpcom/LuaInterpreter/LuaInterpreter.h
+++ trunk/xpcom/LuaInterpreter/LuaInterpreter.h
@@ -10,7 +10,7 @@
 
 #include "ILuaInterpreter.h"
 
-#define LUA_INTERPRETER_CONTRACTID "@mshine.googlecode.com/MoonshineLua;1"
+#define LUA_INTERPRETER_CONTRACTID "@moonshine.luaforge.net/MoonshineLua;1"
 #define LUA_INTERPRETER_CLASSNAME "Embedded Lua interpreter for Mozilla"
 #define LUA_INTERPRETER_CID { 0x37694320, 0x57d6, 0x4056, { 0xbe, 0x3d, 0x24, 0xa1, 0x13, 0x66, 0xa9, 0xb3 } }
 
